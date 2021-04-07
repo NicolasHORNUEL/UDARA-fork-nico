c991f9209be86bd3b66a1d09fe4c4211c6bcc972
nicolasH <nicolas.hornuel@gmail.com>
2021-04-07 16:31:08 +0200

    service commune

---
+++ src/main/java/fr/udara/service/CommuneService.java
@@ -0,0 +1,92 @@
+package fr.udara.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import fr.udara.model.Commune;
+import fr.udara.repository.CommuneRepository;
+
+/**
+ * Classe de service pour l'entité Commune
+ * @author udara
+ *
+ */
+@Service
+public class CommuneService {
+	
+	private CommuneRepository communeRepository;
+
+	/** Constructeur
+	 * 
+	 */
+	@Autowired
+	private CommuneService(CommuneRepository communeRepository) {
+		this.communeRepository = communeRepository;
+	}
+	
+	
+	/**
+	 * @param un objet Commune sans id
+	 * @return l'objet commune avec un id
+	 */
+	public Commune save(Commune commune) {
+		return communeRepository.save(commune);
+		
+	}
+	/**
+	 * @return une liste d'objet commune
+	 */
+	public List<Commune> findAll() {
+		return communeRepository.findAll();
+	}
+
+	
+	/**
+	 * @param id d'un objet commune
+	 * @return une oéventuelle liste d'objet commune
+	 */
+	public Optional<Commune> findById(Long id) {
+		return communeRepository.findById(id);
+	}
+
+	
+	/**
+	 * @param id d'un objet commune
+	 * @return true si l'id existe
+	 */
+	public boolean existsById(Long id) {
+		return communeRepository.existsById(id);
+	}
+	
+	
+	/**
+	 * @return le nombre d'objet commune
+	 */
+	public long count() {
+		return communeRepository.count();
+	}
+	
+	
+	/**
+	 * @param id d'un objet commune
+	 */
+	public void deleteById(Long id) {
+		communeRepository.deleteById(id);
+	}
+	
+	
+	/**
+	 * @param un objet commune
+	 */
+	public void delete(Commune commune) {
+		communeRepository.delete(commune);
+	}
+	
+}
+
+
+
+
