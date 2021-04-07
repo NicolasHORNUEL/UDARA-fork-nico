0781904d1c503639f22fa9bfd843aecb7aaf9d51
’val-dev-g’ <jajora78@gmail.com>
2021-04-07 17:12:11 +0200

    Ajout service pour : indicateurAir, NiveauMeteo et Notification

---
+++ src/main/java/fr/udara/service/IndicateurAirService.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package fr.udara.service;
+
+/**
+ * @author PC VALENTIN
+ *
+ */
+public class IndicateurAirService {
+
+}
---
+++ src/main/java/fr/udara/service/NotificationService.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package fr.udara.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.udara.model.Notification;
+import fr.udara.repository.NotificationRepository;
+
+/**
+ * Classe de service pour l'entité Notification
+ * @author UDARA
+ *
+ */
+
+@Service
+public class NotificationService {
+	
+	private NotificationRepository notificationRepository;
+
+	/** Constructeur
+	 * 
+	 */
+	@Autowired
+	private NotificationService(NotificationRepository notificationRepository) {
+		this.notificationRepository = notificationRepository;
+	}
+	
+	
+	/**
+	 * @param un objet Notification sans id
+	 * @return l'objet Notification avec un id
+	 */
+	@Transactional
+	public Notification save(Notification notification) {
+		return notificationRepository.save(notification);
+		
+	}
+	/**
+	 * @return une liste d'objet Notification
+	 */
+	public List<Notification> findAll() {
+		return notificationRepository.findAll();
+	}
+
+	
+	/**
+	 * @param id d'un objet Notification
+	 * @return une oéventuelle liste d'objet Notification
+	 */
+	public Optional<Notification> findById(Long id) {
+		return notificationRepository.findById(id);
+	}
+
+	
+	/**
+	 * @param id d'un objet Notification
+	 * @return true si l'id existe
+	 */
+	public boolean existsById(Long id) {
+		return notificationRepository.existsById(id);
+	}
+	
+	
+	/**
+	 * @return le nombre d'objet Notification
+	 */
+	public long count() {
+		return notificationRepository.count();
+	}
+	
+	
+	/**
+	 * @param id d'un objet Notification
+	 */
+	@Transactional
+	public void deleteById(Long id) {
+		notificationRepository.deleteById(id);
+	}
+	
+	
+	/**
+	 * @param un objet Notification
+	 */
+	@Transactional
+	public void delete(Notification notification) {
+		notificationRepository.delete(notification);
+	}
+
+}
