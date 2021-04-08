c23fb952a40f22869faafcd516f47c15292a2db2
nicolasH <nicolas.hornuel@gmail.com>
2021-04-08 15:00:30 +0200

    un peu de batch

--- src/main/java/fr/udara/UdaraApplication.java
+++ src/main/java/fr/udara/UdaraApplication.java
@@ -1,18 +1,11 @@
 package fr.udara;
 
-import java.net.URI;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import fr.udara.batch.MeteoData;
+import fr.udara.batch.integrationAPI;
 
 @SpringBootApplication
 @EnableWebMvc
@@ -25,33 +18,7 @@ public class UdaraApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		RestTemplate restTemplate = new RestTemplate();
-		
-		String API_key = "5365db9398346b11fec2438e41e3563b";
-		String city_name = "Montpellier";
-		String metrics = "metrics";
-		String url_Meteo = "https://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&appid=" + API_key + "&units=" + metrics;		
-		URI uri = new URI(url_Meteo);
-		
-		ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
-		String jsonString = response.getBody();
-		
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		
-		MeteoData bean = mapper.readValue(jsonString, MeteoData.class);
-		System.out.println(bean);
-
-		
-		String url_Indicateur = "http://api.openweathermap.org/data/2.5/air_pollution?lat=" + bean.getLatitude() + "&lon=" + bean.getLongitude() + "&appid=" + API_key;
-		URI uri_Indicateur = new URI(url_Indicateur);
-		ResponseEntity<String> response_Indicateur = restTemplate.getForEntity(uri_Indicateur, String.class);
-		String jsonString_Indicateur = response_Indicateur.getBody();
-
-		
-		MeteoData bean_Indicateur = mapper.readValue(jsonString_Indicateur, MeteoData.class);
-		System.out.println(bean_Indicateur);
-		
+		integrationAPI.traite();	
 	}
 
 	
--- src/main/java/fr/udara/batch/IndicateurData.java
+++ src/main/java/fr/udara/batch/IndicateurData.java
@@ -1,18 +1,183 @@
 package fr.udara.batch;
 
-public class IndicateurData {
+import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Classe POJO de données d'indicateur 
+ * @author udara
+ *
+ */
+public class IndicateurData {
+	
+	/** Сoncentration of CO (Carbon monoxide), μg/m3 : Float */
+	private Float co;
+	/** Сoncentration of NO (Nitrogen monoxide), μg/m3 : Float */
+	private Float no;
+	/** Сoncentration of NO2 (Nitrogen dioxide), μg/m3 : Float */
+	private Float no2;
+	/** Сoncentration of O3 (Ozone), μg/m3 : Float */
+	private Float o3;
+	/** Сoncentration of SO2 (Sulphur dioxide), μg/m3 : Float */
+	private Float so2;
+	/** Сoncentration of PM2.5 (Fine particles matter), μg/m3 : Float */
+	private Float pm2_5;
+	/** Сoncentration of PM10 (Coarse particulate matter), μg/m3 : Float */
+	private Float pm10;
+	/** Сoncentration of NH3 (Ammonia), μg/m3 : Float */
+	private Float nh3;
 	
 	/**
-	 * 
+	 * Obtenir la valeur des clés "co","no","no2","o3","so2","pm2_5","pm10","nh3"
+	 * @param objet components
 	 */
-	private String co;
-	private String no;
-	private String no2;
-	private String o3;
-	private String so2;
-	private String pm2_5;
-	private String pm10;
-	private String nh3;
+	@JsonProperty("components")
+	private void nested_co(Map<String, Float> components) {
+		this.co = components.get("co");
+		this.no = components.get("no");
+		this.no2 = components.get("no2");
+		this.o3 = components.get("o3");
+		this.so2 = components.get("so2");
+		this.pm2_5 = components.get("pm2_5");
+		this.pm10 = components.get("pm10");
+		this.nh3 = components.get("nh3");
+	}
 
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("IndicateurData [co=");
+		builder.append(co);
+		builder.append(", no=");
+		builder.append(no);
+		builder.append(", no2=");
+		builder.append(no2);
+		builder.append(", o3=");
+		builder.append(o3);
+		builder.append(", so2=");
+		builder.append(so2);
+		builder.append(", pm2_5=");
+		builder.append(pm2_5);
+		builder.append(", pm10=");
+		builder.append(pm10);
+		builder.append(", nh3=");
+		builder.append(nh3);
+		builder.append("]");
+		return builder.toString();
+	}
+
+	/** Getter
+	 * @return the co
+	 */
+	public Float getCo() {
+		return co;
+	}
+
+	/** Setter
+	 * @param co the co to set
+	 */
+	public void setCo(Float co) {
+		this.co = co;
+	}
+
+	/** Getter
+	 * @return the no
+	 */
+	public Float getNo() {
+		return no;
+	}
+
+	/** Setter
+	 * @param no the no to set
+	 */
+	public void setNo(Float no) {
+		this.no = no;
+	}
+
+	/** Getter
+	 * @return the no2
+	 */
+	public Float getNo2() {
+		return no2;
+	}
+
+	/** Setter
+	 * @param no2 the no2 to set
+	 */
+	public void setNo2(Float no2) {
+		this.no2 = no2;
+	}
+
+	/** Getter
+	 * @return the o3
+	 */
+	public Float getO3() {
+		return o3;
+	}
+
+	/** Setter
+	 * @param o3 the o3 to set
+	 */
+	public void setO3(Float o3) {
+		this.o3 = o3;
+	}
+
+	/** Getter
+	 * @return the so2
+	 */
+	public Float getSo2() {
+		return so2;
+	}
+
+	/** Setter
+	 * @param so2 the so2 to set
+	 */
+	public void setSo2(Float so2) {
+		this.so2 = so2;
+	}
+
+	/** Getter
+	 * @return the pm25
+	 */
+	public Float getPm25() {
+		return pm2_5;
+	}
+
+	/** Setter
+	 * @param pm25 the pm25 to set
+	 */
+	public void setPm25(Float pm25) {
+		this.pm2_5 = pm25;
+	}
+
+	/** Getter
+	 * @return the pm10
+	 */
+	public Float getPm10() {
+		return pm10;
+	}
+
+	/** Setter
+	 * @param pm10 the pm10 to set
+	 */
+	public void setPm10(Float pm10) {
+		this.pm10 = pm10;
+	}
+
+	/** Getter
+	 * @return the nh3
+	 */
+	public Float getNh3() {
+		return nh3;
+	}
+
+	/** Setter
+	 * @param nh3 the nh3 to set
+	 */
+	public void setNh3(Float nh3) {
+		this.nh3 = nh3;
+	}
+	
+	
 }
