044035e82161adab632d0439cab8d5f0b23ae56c
nicolasH <nicolas.hornuel@gmail.com>
2021-04-08 10:20:05 +0200

    ajout import notif repo

--- src/main/java/fr/udara/repository/NotificationRepository.java
+++ src/main/java/fr/udara/repository/NotificationRepository.java
@@ -5,6 +5,8 @@ package fr.udara.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import fr.udara.model.Notification;
+
 /**
  * Interface implémente des méthodes CRUD de JPA repository
  * @author UDARA
