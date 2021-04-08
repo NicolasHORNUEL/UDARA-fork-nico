2a6f9562ab749d07bfe7303adb744b6263e43431
nicolasH <nicolas.hornuel@gmail.com>
2021-04-08 11:55:27 +0200

    resTemplate

---
+++ src/main/java/fr/udara/batch/IndicateurData.java
@@ -0,0 +1,14 @@
+package fr.udara.batch;
+
+public class IndicateurData {
+
+	private String co;
+	private String no;
+	private String no2;
+	private String o3;
+	private String so2;
+	private String pm2_5;
+	private String pm10;
+	private String nh3;
+
+}
