f4e6bb3f4fc2a7b3fe0db7033d3230c2a92d622a
nicolasH <nicolas.hornuel@gmail.com>
2021-04-08 20:13:14 +0200

    integr. recensement

--- src/main/java/fr/udara/batch/IntegrationRecensement.java
+++ src/main/java/fr/udara/batch/IntegrationRecensement.java
@@ -4,28 +4,34 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import fr.udara.model.Commune;
 import fr.udara.service.CommuneService;
 
-//@Service
+@Service
 public class IntegrationRecensement {
 
-	@Autowired
 	public static CommuneService communeService;
+	
+	//@Autowired
+	public IntegrationRecensement(CommuneService communeService) {
+		this.communeService = communeService;
+	}
+	
 	
 	public static void traite() throws Exception {
 
 		Path path = Paths.get("src/main/resources/recensement.csv");
-		boolean estFichier = Files.isRegularFile(path);
-		boolean estLisible = Files.isReadable(path);
-		System.out.println(estFichier);
-		System.out.println(estLisible);
 
 		try {
+			
+			LocalTime time1 = LocalTime.now();
+			
 			List<String> lignes = Files.readAllLines(path);
 			int nbLigne = 0;
 			for (int i = 2; i < lignes.size(); i++) {
@@ -34,6 +40,11 @@ public class IntegrationRecensement {
 				nbLigne++;
 				System.err.println("Ligne " + (i + 1) + " du tableau Excel enregistrée.");
 			}
+			
+			LocalTime time2 = LocalTime.now();
+			Duration duration = Duration.between(time1, time2);
+			System.out.print("Traitement de " + nbLigne + " lignes effectué en " + duration.toMinutes() + " minutes.");
+
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
@@ -45,12 +56,12 @@ public class IntegrationRecensement {
 		String nom = decoupage[6];
 		String departement = decoupage[2];
 		String region = decoupage[1];
-		Integer population = Integer.parseInt(decoupage[9]);
-		System.out.println(nom + departement + region + decoupage[9]);
+		Integer population = Integer.parseInt(decoupage[9].replaceAll(" ", ""));
+		
 		Commune commune = new Commune(nom, departement, region, population);
 		communeService.save(commune);
-
-
+		
 	}
+	
 
 }
