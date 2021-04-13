35bb3883793712b92a07b5ace7aa2b3fa9536838
EtienneUrbano <urbano.etienne@laposte.net>
2021-04-13 14:34:14 +0200

    Ajout methode de désactivation de Spring Security pour effectuer nos tests PostMan

--- src/main/java/fr/udara/security/SecurityConfig.java
+++ src/main/java/fr/udara/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,12 +22,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	/** customUserDetailsService : UserDetailsService */
 	@Autowired
 	@Qualifier("customUserDetailsService")
 	private UserDetailsService customUserDetailsService;
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 //	    http.csrf()
@@ -47,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		.deleteCookies("JSESSIONID")
 //		.logoutUrl("/logout")
 //		.logoutSuccessUrl("/login"); 
-		
+
 //		 http.authorizeRequests()
 //         .antMatchers("/securityNone").permitAll()
 //         .anyRequest().authenticated()
@@ -55,23 +56,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //         .httpBasic();
 
 	}
-	
+
+	@Override
+	public void configure(WebSecurity http) throws Exception {
+		http.ignoring().antMatchers("/**");
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
 		authManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-	
+
 	@Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-	
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
-	
-	
 
 }
