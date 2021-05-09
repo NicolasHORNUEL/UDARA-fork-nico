bb39e96782cb40c55610e686e955b1983f969b62
nicolasH <nicolas.hornuel@gmail.com>
2021-05-09 21:10:49 +0200

    notif crossorigin

--- src/main/java/fr/udara/controller/notif/NotificationController.java
+++ src/main/java/fr/udara/controller/notif/NotificationController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import fr.udara.service.NotificationService;
  * @author UDARA
  *
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/api/notifications")
 public class NotificationController {
